5305dadb775f43f64eb9c313ac166d9628250d74
pateldhriti <135610241+pateldhriti@users.noreply.github.com>
2025-05-22 18:09:22 +0000

    updated index page

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
+<!DOCTYPE html>
 <html>
   <head>
-    <title>PHP Test</title>
+    <title>Dhriti</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+    <h1>Assignment 1</h1>
+    
 
 </html>
